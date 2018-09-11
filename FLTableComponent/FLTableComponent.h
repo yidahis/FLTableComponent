ab176e4f1429da2882e2b2304b7e2814fe6c9c63
yidahis <yiwanjun@yiwanjuns-imac.local>
2018-09-11 18:27:46 +0800

    init

---
+++ FLTableComponent/FLTableComponent.h
@@ -0,0 +1,19 @@
+//
+//  FLTableComponent.h
+//  FLTableComponent
+//
+//  Created by yidahis on 2018/9/11.
+//  Copyright © 2018 yidahis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FLTableComponent.
+FOUNDATION_EXPORT double FLTableComponentVersionNumber;
+
+//! Project version string for FLTableComponent.
+FOUNDATION_EXPORT const unsigned char FLTableComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FLTableComponent/PublicHeader.h>
+
+
